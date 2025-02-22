71cef4af1bf66396a1c9fa4736a02d08359ef4c5
barankcay <barankcay0405@gmail.com>
2025-02-22 22:16:34 +0300

    Refactor solver parameters and data structures for improved flexibility

--- 01_solvers/v1.cpp
+++ 01_solvers/v1.cpp
@@ -3,58 +3,85 @@
 #include <cmath>
 
 using namespace std;
-const int N = 4;
 
-const double lengthX = 12.0;
-const double lengthY = 12.0;
-
-double hx = lengthX / (N + 2);
-double hy = lengthY / (N + 2);
-
-vector<vector<double>> x(N + 3, vector<double>(N + 3));
-vector<vector<double>> y(N + 3, vector<double>(N + 3));
-
-vector<vector<double>> xm(N + 2, vector<double>(N + 2));
-vector<vector<double>> ym(N + 2, vector<double>(N + 2));
-
-vector<vector<double>> u(N + 2, vector<double>(N + 2));
-vector<vector<double>> v(N + 2, vector<double>(N + 2));
-
-vector<vector<double>> p(N + 2, vector<double>(N + 2));
-
-void createCoordinatesXY()
+struct constParameters
 {
-    for (int i = 0; i < N + 3; i++)
+    int Nx;
+    int Ny;
+    double lengthX;
+    double lengthY;
+    double hx = lengthX / (Nx + 2);
+    double hy = lengthY / (Ny + 2);
+    double time;
+    double timeStepSize;
+};
+
+struct fields
+{
+    vector<vector<double>> x;
+    vector<vector<double>> y;
+    vector<vector<double>> xm;
+    vector<vector<double>> ym;
+    vector<vector<double>> u;
+    vector<vector<double>> v;
+    vector<vector<double>> p;
+
+    fields(int Nx, int Ny)
     {
-        for (int j = 0; j < N + 3; j++)
+        x.resize(Nx + 3, vector<double>(Ny + 3));
+        y.resize(Nx + 3, vector<double>(Ny + 3));
+        xm.resize(Nx + 2, vector<double>(Ny + 2));
+        ym.resize(Nx + 2, vector<double>(Ny + 2));
+        u.resize(Nx + 2, vector<double>(Ny + 2));
+        v.resize(Nx + 2, vector<double>(Ny + 2));
+        p.resize(Nx + 2, vector<double>(Ny + 2));
+    }
+};
+
+void createCoordinatesXY(vector<vector<double>> &x, vector<vector<double>> &y, constParameters params)
+{
+    for (int i = 0; i < params.Nx + 3; i++)
+    {
+        for (int j = 0; j < params.Ny + 3; j++)
         {
-            x[i][j] = i * hx;
-            y[i][j] = j * hy;
+            x[i][j] = i * params.hx;
+            y[i][j] = j * params.hy;
         }
     }
 }
-
-void createCoordinatesXYM()
+void createCoordinatesXYM(vector<vector<double>> &xm, vector<vector<double>> &ym, constParameters params)
 {
-    for (int i = 0; i < N + 2; i++)
+    for (int i = 0; i < params.Nx + 2; i++)
     {
-        for (int j = 0; j < N + 2; j++)
+        for (int j = 0; j < params.Ny + 2; j++)
         {
-            xm[i][j] = hx / 2 + i * hx;
-            ym[i][j] = hy / 2 + j * hy;
+            xm[i][j] = params.hx / 2 + i * params.hx;
+            ym[i][j] = params.hy / 2 + j * params.hy;
         }
     }
 }
 
 int main()
 {
-    createCoordinatesXY();
-    createCoordinatesXYM();
-    // for (int i = 0; i < N + 3; i++)
+    constParameters params;
+    params.Nx = 3;
+    params.Ny = 3;
+    params.lengthX = 1.0;
+    params.lengthY = 1.0;
+    params.time = 20;
+    params.timeStepSize = 0.1;
+    params.hx = params.lengthX / (params.Nx + 2);
+    params.hy = params.lengthY / (params.Ny + 2);
+
+    fields field(params.Nx, params.Ny);
+    createCoordinatesXY(field.x, field.y, params);
+    createCoordinatesXYM(field.xm, field.ym, params);
+
+    // for (int i = 0; i < params.Nx + 3; i++)
     // {
-    //     for (int j = 0; j < N + 3; j++)
+    //     for (int j = 0; j < params.Ny + 3; j++)
     //     {
-    //         cout << x[i][j] << "," << y[i][j] << " ";
+    //         cout << field.x[i][j] << "," << field.y[i][j] << " ";
     //     }
     //     cout << endl;
     // }
