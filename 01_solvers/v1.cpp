32b1056af308de0d248d87c6ab7e28f41d6667ad
barankcay <barankcay0405@gmail.com>
2025-02-25 02:16:42 +0300

    Refactor solver field dimensions and boundary conditions for improved accuracy; adjust simulation parameters for enhanced performance

--- 01_solvers/v1.cpp
+++ 01_solvers/v1.cpp
@@ -42,26 +42,26 @@ struct fields
 
     fields(int Nx, int Ny)
     {
-        x.resize(Nx + 3, vector<double>(Ny + 3));
-        y.resize(Ny + 3, vector<double>(Ny + 3));
-        xm.resize(Nx + 2, vector<double>(Ny + 2));
-        ym.resize(Ny + 2, vector<double>(Ny + 2));
-        u.resize(Nx + 2, vector<double>(Ny + 2));
-        uStar.resize(Nx + 2, vector<double>(Ny + 2));
-        uNew.resize(Nx + 2, vector<double>(Ny + 2));
-        vStar.resize(Nx + 2, vector<double>(Ny + 2));
-        vNew.resize(Nx + 2, vector<double>(Ny + 2));
-        v.resize(Nx + 2, vector<double>(Ny + 2));
-        p.resize(Nx + 2, vector<double>(Ny + 2));
+        x.resize(Nx + 1, vector<double>(Ny + 1));
+        y.resize(Nx + 1, vector<double>(Ny + 1));
+        xm.resize(Nx, vector<double>(Ny));
+        ym.resize(Nx, vector<double>(Ny));
+        u.resize(Nx, vector<double>(Ny));
+        uStar.resize(Nx, vector<double>(Ny));
+        uNew.resize(Nx, vector<double>(Ny));
+        vStar.resize(Nx, vector<double>(Ny));
+        vNew.resize(Nx, vector<double>(Ny));
+        v.resize(Nx, vector<double>(Ny));
+        p.resize(Nx, vector<double>(Ny));
     }
 };
 
 // function to create the coordinates of the nodes of cells
 void createCoordinatesXY(vector<vector<double>> &x, vector<vector<double>> &y, constParameters params)
 {
-    for (int i = 0; i < params.Nx + 2; i++)
+    for (int i = 0; i < params.Nx + 1; i++)
     {
-        for (int j = 0; j < params.Ny + 2; j++)
+        for (int j = 0; j < params.Ny + 1; j++)
         {
             x[i][j] = i * params.hx;
             y[i][j] = j * params.hy;
@@ -72,9 +72,9 @@ void createCoordinatesXY(vector<vector<double>> &x, vector<vector<double>> &y, c
 // function to create the coordinates of the cell centers
 void createCoordinatesXYM(vector<vector<double>> &xm, vector<vector<double>> &ym, constParameters params)
 {
-    for (int i = 0; i < params.Nx + 2; i++)
+    for (int i = 0; i < params.Nx; i++)
     {
-        for (int j = 0; j < params.Ny + 2; j++)
+        for (int j = 0; j < params.Ny; j++)
         {
             xm[i][j] = params.hx / 2 + i * params.hx;
             ym[i][j] = params.hy / 2 + j * params.hy;
@@ -127,9 +127,9 @@ double firstOrderPDEbackwardDiff(vector<vector<double>> &variable, int i, int j,
 // PREDICTOR STEP
 void veloctiyStarCalculator(fields &field, constParameters params)
 {
-    for (int i = 1; i < params.Nx + 1; i++)
+    for (int i = 1; i < params.Nx - 1; i++)
     {
-        for (int j = 1; j < params.Ny + 1; j++)
+        for (int j = 1; j < params.Ny - 1; j++)
         {
             field.uStar[i][j] = field.u[i][j] + params.timeStepSize * (params.kinematicViscosity * (secondOrderPDEcentralDiff(field.u, i, j, 1, 0, params) + secondOrderPDEcentralDiff(field.u, i, j, 0, 1, params)) - (field.u[i][j] * firstOrderPDEcentralDiff(field.u, i, j, 1, 0, params) + 0.25 * (field.v[i - 1][j] + field.v[i][j] + field.v[i - 1][j + 1] + field.v[i][j + 1]) * firstOrderPDEcentralDiff(field.u, i, j, 0, 1, params)));
             field.vStar[i][j] = field.v[i][j] + params.timeStepSize * (params.kinematicViscosity * (secondOrderPDEcentralDiff(field.v, i, j, 1, 0, params) + secondOrderPDEcentralDiff(field.v, i, j, 0, 1, params)) - (field.v[i][j] * firstOrderPDEcentralDiff(field.v, i, j, 0, 1, params) + 0.25 * (field.u[i][j - 1] + field.u[i][j] + field.u[i + 1][j - 1] + field.u[i + 1][j]) * firstOrderPDEcentralDiff(field.v, i, j, 1, 0, params)));
@@ -140,11 +140,11 @@ void veloctiyStarCalculator(fields &field, constParameters params)
 
 void poissonEquationSolver(fields &field, constParameters params)
 {
-    for (int k = 0; k < 300; k++)
+    for (int k = 0; k < 10; k++)
     {
-        for (int i = 1; i < params.Nx + 1; i++)
+        for (int i = 1; i < params.Nx - 1; i++)
         {
-            for (int j = 1; j < params.Ny + 1; j++)
+            for (int j = 1; j < params.Ny - 1; j++)
             {
                 field.p[i][j] = (pow(params.hx, 2) * pow(params.hx, 2) / (2 * (pow(params.hx, 2) + pow(params.hy, 2)))) * (((field.p[i + 1][j] + field.p[i - 1][j]) / pow(params.hx, 2)) + ((field.p[i][j + 1] + field.p[i][j - 1]) / pow(params.hy, 2)) - ((params.density / params.timeStepSize) * (firstOrderPDEforwardDiff(field.uStar, i, j, 1, 0, params) + firstOrderPDEforwardDiff(field.vStar, i, j, 0, 1, params))));
             }
@@ -155,9 +155,9 @@ void poissonEquationSolver(fields &field, constParameters params)
 // CORRECTOR STEP
 void velocityCorrector(fields &field, constParameters params)
 {
-    for (int i = 1; i < params.Nx + 1; i++)
+    for (int i = 1; i < params.Nx - 1; i++)
     {
-        for (int j = 1; j < params.Ny + 1; j++)
+        for (int j = 1; j < params.Ny - 1; j++)
         {
             field.uNew[i][j] = field.uStar[i][j] - (params.timeStepSize / params.density) * (firstOrderPDEbackwardDiff(field.p, i, j, 1, 0, params));
             field.vNew[i][j] = field.vStar[i][j] - (params.timeStepSize / params.density) * (firstOrderPDEbackwardDiff(field.p, i, j, 0, 1, params));
@@ -167,9 +167,9 @@ void velocityCorrector(fields &field, constParameters params)
 
 void swapFields(fields &field, constParameters params)
 {
-    for (int i = 1; i < params.Nx + 1; i++)
+    for (int i = 1; i < params.Nx - 1; i++)
     {
-        for (int j = 1; j < params.Ny + 1; j++)
+        for (int j = 1; j < params.Ny - 1; j++)
         {
             field.u[i][j] = field.uNew[i][j];
             field.v[i][j] = field.vNew[i][j];
@@ -179,22 +179,102 @@ void swapFields(fields &field, constParameters params)
 
 void setBoundaryConditions(int b, vector<vector<double>> &M, constParameters params)
 {
-    for (int i = 1; i < params.Nx + 1; i++)
+    if (b == 0)
     {
-        M[i][0] = b == 1 ? 2 * params.uLeftWall - M[i][1] : M[i][1];
-        M[i][params.Ny + 1] = b == 1 ? 2 * params.uRightWall - M[i][params.Ny] : M[i][params.Ny];
+        for (int i = 1; i < params.Nx - 1; i++)
+        {
+            M[i][0] = M[i][1];
+            M[i][params.Ny - 1] = M[i][params.Ny - 2];
+        }
+        for (int j = 1; j < params.Ny - 1; j++)
+        {
+            M[0][j] = 0.0;
+            M[params.Nx - 1][j] = M[params.Nx - 2][j];
+        }
     }
-    for (int j = 1; j < params.Ny + 1; j++)
+    if (b == 1)
     {
-        M[0][j] = b == 2 ? 2 * params.vTopWall - M[1][j] : M[1][j];
-        M[params.Nx + 1][j] = b == 2 ? 2 * params.vBottomWall - M[params.Nx][j] : M[params.Nx][j];
+        for (int i = 1; i < params.Nx - 1; i++)
+        {
+            M[i][0] = 0;
+            M[i][params.Ny - 1] = 0;
+        }
+        for (int j = 1; j < params.Ny - 1; j++)
+        {
+            M[0][j] = 0;
+            M[params.Nx - 1][j] = 0;
+        }
     }
+    if (b == 2)
+    {
+        for (int i = 1; i < params.Nx - 1; i++)
+        {
+            M[i][0] = 0;
+            M[i][params.Ny - 1] = 0;
+        }
+        for (int j = 1; j < params.Ny - 1; j++)
+        {
+            M[0][j] = params.vTopWall;
+            M[params.Nx - 1][j] = 0;
+        }
+    }
+
     M[0][0] = 0.5 * (M[1][0] + M[0][1]);
-    M[params.Nx + 1][0] = 0.5 * (M[params.Nx][0] + M[params.Nx + 1][1]);
-    M[0][params.Ny + 1] = 0.5 * (M[0][params.Ny] + M[1][params.Ny + 1]);
-    M[params.Nx + 1][params.Ny + 1] = 0.5 * (M[params.Nx][params.Ny + 1] + M[params.Nx + 1][params.Ny]);
+    M[params.Nx - 1][0] = 0.5 * (M[params.Nx - 2][0] + M[params.Nx - 1][1]);
+    M[0][params.Ny - 1] = 0.5 * (M[0][params.Ny - 2] + M[1][params.Ny - 1]);
+    M[params.Nx - 1][params.Ny - 1] = 0.5 * (M[params.Nx - 1][params.Ny - 2] + M[params.Nx - 2][params.Ny - 1]);
 }
 
+// void saveToVTK(const fields &field, const constParameters &params, const string &filename)
+// {
+//     ofstream vtkFile(filename);
+//     if (!vtkFile.is_open())
+//     {
+//         cerr << "Error: Unable to open file " << filename << endl;
+//         return;
+//     }
+
+//     // Write the VTK header
+//     vtkFile << "# vtk DataFile Version 3.0\n";
+//     vtkFile << "Flow Field Data\n";
+//     vtkFile << "ASCII\n";
+//     vtkFile << "DATASET STRUCTURED_GRID\n";
+//     vtkFile << "DIMENSIONS " << params.Nx << " " << params.Ny << " 1\n";
+//     vtkFile << "POINTS " << (params.Nx) * (params.Ny) << " float\n";
+
+//     // Write the grid coordinates
+//     for (int j = 0; j < params.Ny; j++)
+//     {
+//         for (int i = 0; i < params.Nx; i++)
+//         {
+//             vtkFile << field.xm[i][j] << " " << field.ym[i][j] << " 0.0\n";
+//         }
+//     }
+
+//     // Write the velocity and pressure data
+//     vtkFile << "POINT_DATA " << (params.Nx) * (params.Ny) << "\n";
+//     vtkFile << "VECTORS velocity float\n";
+//     for (int j = 0; j < params.Ny; j++)
+//     {
+//         for (int i = 0; i < params.Nx; i++)
+//         {
+//             vtkFile << field.u[i][j] << " " << field.v[i][j] << " 0.0\n";
+//         }
+//     }
+
+//     vtkFile << "SCALARS pressure float 1\n";
+//     vtkFile << "LOOKUP_TABLE default\n";
+//     for (int j = 0; j < params.Ny; j++)
+//     {
+//         for (int i = 0; i < params.Nx; i++)
+//         {
+//             vtkFile << field.p[i][j] << "\n";
+//         }
+//     }
+
+//     vtkFile.close();
+//     cout << "Data saved to " << filename << endl;
+// }
 int main()
 {
 
@@ -203,17 +283,17 @@ int main()
     params.density = 1.0;
     params.kinematicViscosity = 0.01;
 
-    params.vTopWall = 0.5;
+    params.vTopWall = 1;
     params.vBottomWall = 0.0;
     params.uLeftWall = 0.0;
     params.uRightWall = 0.0;
 
-    params.Nx = 18;
-    params.Ny = 18;
+    params.Nx = 20;
+    params.Ny = 20;
     params.lengthX = 0.1;
     params.lengthY = 0.1;
-    params.hx = params.lengthX / (params.Nx + 2);
-    params.hy = params.lengthY / (params.Ny + 2);
+    params.hx = params.lengthX / (params.Nx);
+    params.hy = params.lengthY / (params.Ny);
 
     params.startTime = 0;
     params.endTime = 0.5;
@@ -230,9 +310,9 @@ int main()
     for (double t = params.startTime; t < params.endTime; t = t + params.timeStepSize)
     {
 
-        // for (int i = 0; i < params.Nx + 2; i++)
+        // for (int i = 0; i < params.Nx ; i++)
         // {
-        //     for (int j = 0; j < params.Ny + 2; j++)
+        //     for (int j = 0; j < params.Ny; j++)
         //     {
         //         cout << field.p[i][j] << " ";
         //     }
@@ -240,20 +320,26 @@ int main()
         // }
         // cout << endl;
         veloctiyStarCalculator(field, params);
-
         poissonEquationSolver(field, params);
         velocityCorrector(field, params);
         swapFields(field, params);
-        // if (count == params.numberOfTimeSteps - 1)
+        setBoundaryConditions(0, field.p, params);
+        if (count == params.numberOfTimeSteps - 1)
+        {
+            for (int i = 0; i < params.Nx; i++)
+            {
+                cout << field.v[i][params.Ny / 2] << " ";
+            }
+        }
+        // if (count == params.numberOfTimeSteps) // Save every 10 time steps
         // {
-        //     for (int i = 0; i < params.Nx + 2; i++)
-        //     {
-        //         cout << field.v[i][params.Ny / 2] << " ";
-        //     }
+        //     string filename = "output_" + to_string(count) + ".vtk";
+        //     saveToVTK(field, params, filename);
         // }
 
         count++;
     }
     cout << params.timeStepSize << endl;
+    cout << "Simulation completed. Data saved in VTK format." << endl;
     return 0;
 }
